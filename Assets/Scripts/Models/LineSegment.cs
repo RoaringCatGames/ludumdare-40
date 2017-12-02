4cbaf6fcbf7a0069ace7a055dad16c1a1b98e03f
Barryrowe <barryrowe@gmail.com>
2017-12-02 14:45:53 -0500

    Animate the growth of a branch w/ Lerp
    Instead of plopping in the next segment immediately, we lerp the top
    segment into length to create an animation.
    
    We also lerp the base width up  so that the tree really appears to grow.

---
+++ Assets/Scripts/Models/LineSegment.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class LineSegment {
+
+  public LineSegment(){
+    StartPoint = new Vector3();
+    EndPoint = new Vector3();
+  }
+  public LineSegment(Vector3 start, Vector3 end){
+    StartPoint = start;
+    EndPoint = end;
+  }
+
+  public LineSegment(float startX, float startY, float endX, float endY){
+    StartPoint = new Vector3(startX, startY);
+    EndPoint = new Vector3(endX, endY);
+  }
+
+  public Vector3 StartPoint {get; set;}
+  public Vector3 EndPoint {get; set;}
+}
--- Assets/Scripts/TreeBuilder.cs
+++ Assets/Scripts/TreeBuilder.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 
 public class TreeBuilder : MonoBehaviour {
 
+	[Header("Growth Properties")]
 	public float secondsBetweenSegments = 2f;
 	public float maxHeight = 20f;
-	public float maxXOffset = 10f;
-
+	public float maxLeftRightDistance = 10f;
 	public Vector2 xMinMaxChangeMagnitude = new Vector2(0.1f, 1f);
 	public Vector2 yMinMaxChangeMagnitude = new Vector2(0.3f, 1.5f);
+	public float maxBaseWidth = 3f;
+	public float tipWidth = 0.25f;
+
+
+	[HideInInspector]
+	public List<LineSegment> LineSegments = new List<LineSegment>();
 
 	private LineRenderer lineRenderer;
-
 	private float elapsedTime = 0f;
+	private Vector3 nextTargetPosition;
 
 	// Use this for initialization
 	void Awake () {
@@ -26,42 +32,66 @@ public class TreeBuilder : MonoBehaviour {
 				new Vector3(0, 1, 0)
 			};
 
-			//lineRenderer.widthMultiplier = 0.1f; 
+			lineRenderer.widthMultiplier = 1f;  		
 			lineRenderer.positionCount = positions.Length;
 			lineRenderer.SetPositions(positions);
+
+			LineSegments.Add(new LineSegment(positions[0], positions[1]));
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		elapsedTime += Time.deltaTime;
-		if(elapsedTime >= secondsBetweenSegments){
-			elapsedTime = elapsedTime%secondsBetweenSegments;
-			
-			//Setup Next Position
-			Vector3 lastPosition = lineRenderer.GetPosition(lineRenderer.positionCount - 1);
-			Vector3 nextPos = generateNextPoint(lastPosition);	
-			
-			//Build new Positions Array
-			Vector3[] linePositions = new Vector3[lineRenderer.positionCount + 1];
-			int positionsReturned = lineRenderer.GetPositions(linePositions);
-			linePositions[linePositions.Length - 1] = nextPos;
+		// Throttle updates to 60fps
+		float throttledDeltaTime = Mathf.Clamp(Time.deltaTime, 0, 1f/60f);
 
-			//Reset Positions
-			lineRenderer.positionCount = linePositions.Length;
-			lineRenderer.SetPositions(linePositions);			
+	
+		Vector3 topPosition = lineRenderer.GetPosition(lineRenderer.positionCount - 1);
+		Vector3 prevPosition = lineRenderer.GetPosition(lineRenderer.positionCount - 2);
+		Vector3[] linePositions;
+		Vector3 newTopPosition;
+		int linePositionsSize = lineRenderer.positionCount;
+
+		if(nextTargetPosition == null || topPosition.x == nextTargetPosition.x && topPosition.y == nextTargetPosition.y) {
+			nextTargetPosition = generateNextPoint(topPosition);
+			//Add a new position to our linePositions
+			linePositionsSize += 1;			
+			//Duplicate Top Position
+			newTopPosition = topPosition;
+			LineSegments.Add(new LineSegment(topPosition, newTopPosition));
+
+			//TODO: Update animation Curve based on size
+			elapsedTime = 0f;
+		}else{
+			// Lerp to Target!!
+			elapsedTime += throttledDeltaTime;
+			float positionOfGrowthTime = elapsedTime/secondsBetweenSegments;
+			newTopPosition = Vector3.Lerp(prevPosition, nextTargetPosition, positionOfGrowthTime);
+			
+			//Update our last Line Segment
+			LineSegments.Last().EndPoint = newTopPosition;						
 		}
+
+		//Reset Positions
+		linePositions = new Vector3[linePositionsSize];
+		lineRenderer.GetPositions(linePositions);
+		linePositions[linePositions.Length - 1] = newTopPosition;
+		lineRenderer.positionCount = linePositions.Length;
+		float baseWidth = Mathf.Clamp((Vector3.Distance(newTopPosition, linePositions[0])/maxHeight) * maxBaseWidth, tipWidth, maxBaseWidth);
+		lineRenderer.widthCurve = new AnimationCurve(
+			new Keyframe(0f, baseWidth),
+			new Keyframe(1f, tipWidth)
+		);
+		lineRenderer.SetPositions(linePositions);	
 	}
 
 	private Vector3 generateNextPoint(Vector3 lastPoint){
 		float nextX, nextY;
 		int direction = Random.Range(0f, 1f) >= 0.5f ? 1 : -1;
 		nextX = Random.Range(direction * xMinMaxChangeMagnitude.x, direction * xMinMaxChangeMagnitude.y) + lastPoint.x;
-		nextX = Mathf.Clamp(nextX, -maxXOffset, maxXOffset);
+		nextX = Mathf.Clamp(nextX, -maxLeftRightDistance, maxLeftRightDistance);
 		nextY = Random.Range(yMinMaxChangeMagnitude.x, yMinMaxChangeMagnitude.y) + lastPoint.y;
-		nextY = Mathf.Clamp(nextY, 0f, maxHeight);
-
-		Logger.Log("Next X: ",  nextX , "Next Y: ", nextY);
+		nextY = Mathf.Clamp(nextY, 0f, maxHeight);		
 		return new Vector3(
 			nextX,
 			nextY,
