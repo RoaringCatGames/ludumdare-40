01fac2db74d0233fb89ce0e4f686110ac1d8297b
Barryrowe <barryrowe@gmail.com>
2017-12-04 18:50:26 -0500

    Setup Game Navigation
    Setting up a menu and instruction screen that need to get loaded.

---
+++ Assets/Scripts/MenuNavigationController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigationController : MonoBehaviour {
+
+	public string sceneName = "MenuScreen";
+
+	
+	public void TransitionToScene(){
+		SceneManager.LoadScene(sceneName);
+	}
+}
