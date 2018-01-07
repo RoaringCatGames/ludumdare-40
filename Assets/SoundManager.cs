cd9ddb366b102211ba06ddaf2fd999e578c9d306
Barryrowe <barryrowe@gmail.com>
2018-01-06 23:34:50 -0500

    Add camera SFX when camera button pressed
    When we start the snapshot process we fire off the SFX for the camera.
    
    This closes #7

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class KeyAudioClipPair {
+	public string key;
+	public AudioClip clip;
+}
+
 public class SoundManager : MonoBehaviour {
 
 	public static SoundManager instance;
@@ -9,15 +15,25 @@ public class SoundManager : MonoBehaviour {
 	public AudioClip[] ambientSfx;
 	public AudioClip[] bloops;
 
+	public List<KeyAudioClipPair> soundEffects;
+
 	public AudioSource music;
 	public AudioSource sfx;
 
+	private Dictionary<string, AudioClip> sfxMap;
 	// Use this for initialization
 	void Start () {
 
 		if(instance == null){
 			instance = this;
 			sfx.loop = false;
+			// Inititialze our audio clip map so we can
+			//	access them quickly.
+			sfxMap = new Dictionary<string, AudioClip>();
+			foreach(KeyAudioClipPair kacp in soundEffects){
+				sfxMap.Add(kacp.key.ToLower(), kacp.clip);
+			}
+
 			PlayBackgroundMusic();
 		}else if(instance != this){
 			Destroy(gameObject);
@@ -39,5 +55,9 @@ public class SoundManager : MonoBehaviour {
 
 	public void PlayRandomBloopSfx(){
 		sfx.PlayOneShot(bloops[Random.Range(0, bloops.Length)], 0.25f);
+	}
+
+	public void PlaySfxByName(string sfxKey){
+		sfx.PlayOneShot(sfxMap[sfxKey.ToLower()], 1.0f); 
 	}
 }
