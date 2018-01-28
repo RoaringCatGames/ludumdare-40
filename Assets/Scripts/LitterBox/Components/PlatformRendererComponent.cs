a6f7bfd3342d22b016bdb3fbb0d85f20a8c6481f
Barryrowe <barryrowe@gmail.com>
2018-01-28 18:28:24 -0500

    Hide Twitter Integration on Web
    The twitter integration on web is now hidden. Will need to do the
    same on the screenshot capture eventually.

---
+++ Assets/Scripts/LitterBox/Components/PlatformRendererComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRendererComponent : MonoBehaviour {
+
+	public RuntimePlatform[] platformsToHide;
+
+	// Use this for initialization
+	void Start () {
+		if(platformsToHide != null && platformsToHide.Length > 0){
+			foreach(RuntimePlatform p in platformsToHide) {
+				if(p == Application.platform) {
+					gameObject.SetActive(false);
+				}
+			}
+		}
+	}
+}
