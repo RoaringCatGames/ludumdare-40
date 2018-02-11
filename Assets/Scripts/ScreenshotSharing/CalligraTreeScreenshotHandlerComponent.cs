8eb40703894ae0deb813e1d684af6e01c240a05c
Barryrowe <barryrowe@gmail.com>
2018-02-11 11:55:19 -0500

    Adding image saved slideout icon
    When a snapshot is taken, the image saved icon slides out.

--- Assets/Scripts/ScreenshotSharing/CalligraTreeScreenshotHandlerComponent.cs
+++ Assets/Scripts/ScreenshotSharing/CalligraTreeScreenshotHandlerComponent.cs
@@ -5,6 +5,8 @@ using LitterBox.Utils;
 public class CalligraTreeScreenshotHandlerComponent: MonoBehaviour {
 
   public GameObject twitterUI;
+  public Animator animatorToTrigger;
+  public string animationNameToPlay;
 
   public void StartTwitterSharingProcess(Texture2D screenshot) {
 
@@ -19,9 +21,18 @@ public class CalligraTreeScreenshotHandlerComponent: MonoBehaviour {
   public void SaveToGallery(Texture2D screenshot) {
     #if UNITY_IOS || UNITY_ANDROID
     // Save to Gallery!
-    NativeGallery.SaveToGallery(screenshot, "CalligraTrees", "calligratree-{0}.png");
-    
+    NativeGallery.SaveToGallery(screenshot, "CalligraTrees", "calligratree-{0}.png");        
     #endif
+    this._triggerAnimation();
     Kitten.Meow("SAVE TO GALLERY FIRED!");
+  }
+
+  private void _triggerAnimation() {
+    if(animatorToTrigger != null && !String.IsNullOrEmpty(animationNameToPlay)) {
+      Kitten.Meow("Trigger Animation: ", animationNameToPlay);
+      animatorToTrigger.Play(animationNameToPlay);      
+    }else {
+      Kitten.Meow("Animator empty or animation name to play Empty", animatorToTrigger, animationNameToPlay);
+    }
   }
 }
